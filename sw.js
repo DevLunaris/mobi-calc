b3fde750b875a9637cc65c5614f217350e3a028c
LunaDev <67156179+DevLunaris@users.noreply.github.com>
2026-02-27 10:20:22 +0100

    PWA Support | simpler UI
    Progressive Web App und simples UI, für Mobi UND/ODER normale Arbeitszeit

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = "zeit-pwa-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(event.request, copy)).catch(()=>{});
+        return res;
+      }).catch(()=>cached);
+    })
+  );
+});
